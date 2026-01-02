cd5bcbc79368a947103bdd4b894e7a7848dac1da
Deividgaston <157326724+Deividgaston@users.noreply.github.com>
2026-01-02 09:05:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto es vital para que cargue bien en GitHub Pages
+  // ✅ GitHub Pages: https://deividgaston.github.io/FACTURACION-/
+  base: '/FACTURACION-/',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
